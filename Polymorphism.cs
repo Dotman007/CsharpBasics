7a4c167c3ef095a6e2ceafc0ec4d4e774a041bfd
Salami Adedotun <dotman007@outlook.com>
2019-04-26 02:23:10 +0100

    polymorphism

---
+++ Polymorphism.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    public class Polymorphism
+    {
+        public string FirstName = "FN";
+        public string LastName = "LN";
+
+        public virtual void PrintFullInfo()
+        {
+            Console.WriteLine(FirstName + " "+ LastName);
+        }
+    }
+
+    public class FullTimeEmloyee : Polymorphism
+    {
+        
+        public override void PrintFullInfo()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    public class NotFullTimeEmployee : Polymorphism
+    {
+        
+        public override void PrintFullInfo()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    public class TemporaryEmployee : Polymorphism
+    {
+        public override void PrintFullInfo()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,15 +10,26 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            PartTimeEmployee partTimeEmployee = new PartTimeEmployee();
-            Console.WriteLine("This Belongs to my Father : " + partTimeEmployee.FirstName +" " + partTimeEmployee.LastName + " " +partTimeEmployee.Email);
-            partTimeEmployee.GetFullInfo(partTimeEmployee);
-            //Console.WriteLine("This belongs to me alone :" + partTimeEmployee.HourlySalary);
-            Console.WriteLine("======================================="+'\n');
-            Employee fullTimeEmployee = new FullTimeEmployee();
-            Console.WriteLine("This Belongs to my Father : " + fullTimeEmployee.FirstName + " " + fullTimeEmployee.LastName + " " + fullTimeEmployee.Email);
-            fullTimeEmployee.GetFullInfo(fullTimeEmployee);
-            //Console.WriteLine("This belongs to me alone :" + fullTimeEmployee.MonthlySalary);
+            //PartTimeEmployee partTimeEmployee = new PartTimeEmployee();
+            //Console.WriteLine("This Belongs to my Father : " + partTimeEmployee.FirstName +" " + partTimeEmployee.LastName + " " +partTimeEmployee.Email);
+            //partTimeEmployee.GetFullInfo(partTimeEmployee);
+            ////Console.WriteLine("This belongs to me alone :" + partTimeEmployee.HourlySalary);
+            //Console.WriteLine("======================================="+'\n');
+            //Employee fullTimeEmployee = new FullTimeEmployee();
+            //Console.WriteLine("This Belongs to my Father : " + fullTimeEmployee.FirstName + " " + fullTimeEmployee.LastName + " " + fullTimeEmployee.Email);
+            //fullTimeEmployee.GetFullInfo(fullTimeEmployee);
+            ////Console.WriteLine("This belongs to me alone :" + fullTimeEmployee.MonthlySalary);
+            ///
+
+            Polymorphism[] polymorphisms = new Polymorphism[4];
+            polymorphisms[0] = new Polymorphism();
+            polymorphisms[1] = new Polymorphism();
+            polymorphisms[2] = new Polymorphism();
+            polymorphisms[3] = new Polymorphism();
+            foreach (Polymorphism p in polymorphisms)
+            {
+                p.PrintFullInfo();
+            }
             Console.ReadLine();
         }
     }
